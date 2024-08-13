ac5931ef749a177944a9e6f564a036401bc2edca
abali <abali@bu.edu>
2024-08-13 07:16:14 -0400

    Functional edits

--- table.js
+++ table.js
@@ -258,7 +258,7 @@ d3.select('fieldset').on('change', function () {
     var hidden_column_names = [];
 
     for (var name in window.dataset[0]) {
-        if (!hidden_columns.includes(name)) {
+        if (!hidden_columns.includes(parseInt(name))) {
             hidden_column_names.push(window.dataset[0][name]);
         }
     }
